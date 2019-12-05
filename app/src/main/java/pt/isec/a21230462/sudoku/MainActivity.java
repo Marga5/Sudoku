fa7a955403b82fad2be72108eb7cbc78a424b1da
Marcos Gaspar <45184854+Marga5@users.noreply.github.com>
2019-12-05 11:49:43 +0000

    Criação do Projeto

---
+++ app/src/main/java/pt/isec/a21230462/sudoku/MainActivity.java
@@ -0,0 +1,28 @@
+package pt.isec.a21230462.sudoku;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onM1(View view) {
+        //Intent intent = new Intent(this, M1Activity.class);
+        //startActivity(intent);
+    }
+
+    public void onM2(View view) {
+        //Intent intent = new Intent(this, M2Activity.class);
+        //startActivity(intent);
+    }
+}
+
