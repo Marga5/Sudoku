faa7d7113f3f45d8388bfdbaaa227bb33d2a2067
Marcos Gaspar <45184854+Marga5@users.noreply.github.com>
2019-12-05 12:18:24 +0000

    Modificaçoes nos ficheiros de dificuldade e tabuleiro

--- app/src/main/java/pt/isec/a21230462/sudoku/EscolherDificuldade.java
+++ app/src/main/java/pt/isec/a21230462/sudoku/EscolherDificuldade.java
@@ -2,7 +2,9 @@ package pt.isec.a21230462.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EscolherDificuldade extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class EscolherDificuldade extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escolher_dificuldade);
+    }
+
+    public void onFacil(View view){
+        Intent intent = new Intent(this, TabuleiroJogo.class);
+        startActivity(intent);
     }
 }
